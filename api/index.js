1152bb4e380ff015730d5ca092d28c7d84b86203
Andrei Serban <me@andreiserban.com>
2025-06-21 21:58:08 +0300

    api + pdf (great)

---
+++ api/index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+const PORT = 3020;
+
+// Middleware (optional example)
+app.use(express.json());
+
+// Routes
+app.get('/', async (req, res) => {
+
+  console.log('pune la balina');
+
+  // (async () => {
+    // Load HTML file
+    const filePath = path.join(__dirname, 'template.html');
+    const html = fs.readFileSync(filePath, 'utf8');
+
+    // Launch browser
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Set content
+    await page.setContent(html, { waitUntil: 'load' });
+
+    // Generate PDF
+    await page.pdf({
+      path: 'output.pdf',
+      format: 'A4',
+      printBackground: true,
+      margin: { top: '40px', bottom: '40px', left: '40px', right: '40px' }
+    });
+
+    await browser.close();
+
+    res.send('✅ PDF created: output.pdf');
+  // })();
+
+  // res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
